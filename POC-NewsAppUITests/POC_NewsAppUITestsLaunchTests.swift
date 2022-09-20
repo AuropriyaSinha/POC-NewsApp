91213ad71af250a69e3c6b2123ef84772845bae5
Auropriya Sinha <auropriyasinha241995@gmail.com>
2022-09-20 22:11:22 +0530

    Initial Commit

---
+++ POC-NewsAppUITests/POC_NewsAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  POC_NewsAppUITestsLaunchTests.swift
+//  POC-NewsAppUITests
+//
+//  Created by Auropriya Sinha on 20/09/22.
+//
+
+import XCTest
+
+class POC_NewsAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
